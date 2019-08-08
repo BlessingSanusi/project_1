8215f0480699bb54534e6d0b2198a2a1fae32539
Washington <wmashingaidze@gmail.com>
2019-08-08 16:32:52 -0500

    added code for Yelp top events Ajax API

--- javascript/app.js
+++ javascript/app.js
@@ -1,68 +1,32 @@
-var response;
-
 $(document).ready(function() {
 
-//Initialize Firebase
-var config = {
-    apiKey: "AIzaSyB4VocKAGdBV2MOoa4Tlea2kodPc7mg3HQ",
-    authDomain: "travelapp-e9f01.firebaseapp.com",
-    databaseURL: "https://travelapp-e9f01.firebaseio.com",
-    projectId: "travelapp-e9f01",
-    storageBucket: "",
-    messagingSenderId: "986030707385",
-    appId: "1:986030707385:web:0a04b09b94708972"
-  };
-  
-  firebase.initializeApp(config);
-  
-  var database = firebase.database();
 
 
-        function getTravelAdvisoryInfo(){
-
-            //var location = $('#autocomplete-input');
-            var location = 'GR';
-            var api_key = "775e2f93afmsh982d2aa4d4c5505p181e67jsn80b576927669";
-            var queryURL = "https://www.travel-advisory.info/api?countrycode=" + location ;
-
-            $.ajax({
-                url:queryURL,
-                method :"GET"
-            }).then(function(response2){
-                // console.log(response)
-                    
-                    // console.log(results['advisory'])
-                    response = response2;
-                    console.log(response.data[location].advisory)
+    var config = {
+        apiKey: "AIzaSyB4VocKAGdBV2MOoa4Tlea2kodPc7mg3HQ",
+        authDomain: "travelapp-e9f01.firebaseapp.com",
+        databaseURL: "https://travelapp-e9f01.firebaseio.com",
+        projectId: "travelapp-e9f01",
+        storageBucket: "",
+        messagingSenderId: "986030707385",
+        appId: "1:986030707385:web:0a04b09b94708972"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
     
-                })
-                
-            }//end displayTravelAdvisory function  
-         
-            //test displayTravelAdvisory function
-            getTravelAdvisoryInfo()
+            var place = "New York";
+            var limit = 10
+                var settings = {
+                "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?location="+place+"&limit="+limit,
+                "method": "GET",
+                "headers": {
+                "Authorization": "Bearer 0RNHafMEpqhc0LGydMyazrFx-E5hu7sAda5jw9c7No27dJA4wtsXxsxHWmtVi7dPa9ewyFHmR24sl4gV_mQw_vpRSEA_XjnXP-rz77HxPd02FdwmRtfDuxSybF1MXXYx",
+                }
+            }
+            
+            $.ajax(settings).then(function (response) {
+                console.log(response);
+                console.log(response.events[0]);
+            });
 
-
-        //  function getThingsToDo(){
-                       
-        //     var location = 'GR';
-        //     var api_key = "775e2f93afmsh982d2aa4d4c5505p181e67jsn80b576927669";
-        //     var queryURL = "https://api.yelp.com/v3/events/featured" + location ;
-
-        //     $.ajax({
-        //         url:queryURL,
-        //         method :"GET"
-        //     }).then(function(response){
-        //             var results = response.data;
-        //             console.log(results['advisory'])
-    
-        //         })
-
-
-
-        //  }//end getThingsToDo function
-
-
-
-
-    }); //end document.ready
+}); //end document.reapdy
