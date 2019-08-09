71de409b3d509755bde186d71f26c9e5c9509748
Washington <wmashingaidze@gmail.com>
2019-08-08 19:22:03 -0500

    added yelp jax call

--- javascript/app.js
+++ javascript/app.js
@@ -30,3 +30,46 @@ $(document).ready(function() {
             });
 
 }); //end document.reapdy
+
+
+// {
+//     "events": [
+//         {
+//             "attending_count": 1,
+//             "category": "other",
+//             "cost": null,
+//             "cost_max": null,
+//             "description": "THIS EVENT IS FULL TO CAPACITY!  WE ARE NO LONGER ACCEPTING RSVPs.  ABSOLUTELY NO WALK-INS!\n\n*****In light of the recent tragedy in Haiti, all funds...",
+//             "event_site_url": "https://www.yelp.com/events/london-yelps-burst-birthday-event-full?adjust_creative=Y_K0fV6ocULU7tdvseG9Mw&utm_campaign=yelp_api_v3&utm_medium=api_v3_event_search&utm_source=Y_K0fV6ocULU7tdvseG9Mw",
+//             "id": "london-yelps-burst-birthday-event-full",
+//             "image_url": "https://s3-media2.fl.yelpcdn.com/ephoto/lSNudwrOHLFqwCP5ZG1QLw/o.jpg",
+//             "interested_count": 337,
+//             "is_canceled": false,
+//             "is_free": true,
+//             "is_official": false,
+//             "latitude": 51.5325189,
+//             "longitude": -0.1049946,
+//             "name": "Yelp's \"Burst\" Birthday - EVENT FULL",
+//             "tickets_url": "",
+//             "time_end": "2010-01-26T22:00:00+00:00",
+//             "time_start": "2010-01-26T20:00:00+00:00",
+//             "location": {
+//                 "address1": "7 Torrens St",
+//                 "address2": "",
+//                 "address3": "",
+//                 "city": "London",
+//                 "zip_code": "EC1V 1NQ",
+//                 "country": "GB",
+//                 "state": "XGL",
+//                 "display_address": [
+//                     "7 Torrens St",
+//                     "London EC1V 1NQ",
+//                     "United Kingdom"
+//                 ],
+//                 "cross_streets": ""
+//             },
+//             "business_id": "islington-metal-works-london"
+//         }
+//     ],
+//     "total": 1
+// }
